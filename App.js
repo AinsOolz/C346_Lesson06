cf486653ae73ca85f81cba35c0cb4be34e39ad9f
KhairulNabil <24049156@myrp.edu.sg>
2025-11-08 12:08:43 +0800

    Version04 - i noticed i skipped version 02 it was a mistake

--- App.js
+++ App.js
@@ -1,102 +1,90 @@
-import { Text, View, Image, ScrollView, TouchableOpacity, ToastAndroid } from 'react-native';
-import { Picker } from '@react-native-picker/picker';
-import React, { useState } from "react";
+import {Text, TextInput, View, Image, ScrollView, ToastAndroid, TouchableOpacity} from 'react-native';
+import {Picker} from '@react-native-picker/picker';
+import React, {useState} from "react";
 
 export default function App() {
     return (
         <View>
-            <Display />
-        </View>
-    );
+            <Display/>
+        </View>);
 }
+// const InputBox = ({label, onChangeText}) => {
+//     return (
+//         <View>
+//             <Text>{label}</Text>
+//             <TextInput style={{borderWidth: 1}} onChangeText={onChangeText}/>
+//         </View>
+//     );
+// };
 
 function Header() {
     return (
-        <Text style={{ marginTop: 40, marginBottom: 10, textAlign: 'center', fontSize: 30, fontWeight: 'bold' }}>
-            Pokemon Knowledge Quiz!
-        </Text>
-    );
+        <Text style={{marginTop: 40, marginBottom: 10, textAlign: 'center', fontSize: 30, fontWeight: 'bold'}}>Pokemon
+            Knowledge Quiz!</Text>)
 }
 
-function Question({ img, options, index, onSelect }) {
+function Question({img, opt1, opt2, opt3, onSelect}) {
+    // const [type, setType] = useState('');
+    function handleSelect(value) {
+        onSelect(value); // Tell parent what was selected
+    }
+
     return (
         <View>
-            <Image
-                source={img}
-                style={{ width: 410, height: 470, marginBottom: 20 }}
-                resizeMode="contain"
-            />
-
-            <Text style={{ fontSize: 15 }}>Pokemon Name:</Text>
-            <Picker onValueChange={(value) => onSelect(index, value)}>
-                <Picker.Item label="Select an answer" value="" />
-                {options.map((opt, i) => (
-                    <Picker.Item key={i} label={opt} value={opt} />
-                ))}
+            <Image source={img} style={{width: 410, height: 470, marginBottom: 20}} resizeMode="contain"/>
+            <Text style={{fontSize: 15}}>Pokemon Name:</Text>
+            <Picker onValueChange={handleSelect}>
+                <Picker.Item label='Pick an Answer' value=''/>
+                <Picker.Item label={opt1} value={opt1}/>
+                <Picker.Item label={opt2} value={opt2}/>
+                <Picker.Item label={opt3} value={opt3}/>
             </Picker>
-        </View>
-    );
+        </View>)
 }
 
 function Display() {
-    const questions = [
-        {
-            img: require('./img/Camerupt.png'),
-            options: ['Camerupt', 'Pikachu', 'Charizard'],
-            correct: 'Camerupt'
-        },
-        {
-            img: require('./img/Gumshoo.png'),
-            options: ['Pikachu', 'Linoone', 'Gumshoo'],
-            correct: 'Gumshoo'
-        },
-        {
-            img: require('./img/Snom.png'),
-            options: ['Bruxish', 'Snom', 'Pikachu'],
-            correct: 'Snom'
-        }
-    ];
+    const correctAnswers = ['Camerupt', 'Gumshoo', 'Snom'];
+    const [answers, setAnswers] = useState(['', '', '']);
 
-    const [answers, setAnswers] = useState(Array(questions.length).fill(""));
-
-    const handleSelect = (index, value) => {
+    const updateAnswer = (index, answer) => {
         const updated = [...answers];
-        updated[index] = value;
+        updated[index] = answer;
         setAnswers(updated);
     };
 
-    const checkScore = () => {
+    const checkAnswers = () => {
         let score = 0;
-
-        for (let i = 0; i < questions.length; i++) {
-            if (answers[i] === questions[i].correct) {
-                score++;
-            }
-        }
-
-        ToastAndroid.show(`You scored ${score} out of ${questions.length}!`, ToastAndroid.SHORT);
+        answers.forEach((ans, i) => {
+            if (ans === correctAnswers[i]) score++;
+        });
+        ToastAndroid.show(`Score: ${score}/${correctAnswers.length}`, ToastAndroid.SHORT);
     };
 
     return (
         <ScrollView>
-            <Header />
+            <Header/>
+            <Question img={require('./img/Camerupt.png')}
+                      opt1='Camerupt'
+                      opt2='Pikachu'
+                      opt3='Charizard'
+                      onSelect={(answer) => updateAnswer(0, answer)}
+            />
+            <Question img={require('./img/Gumshoo.png')}
+                      opt1='Pikachu'
+                      opt2='Linoone'
+                      opt3='Gumshoo'
+                      onSelect={(answer) => updateAnswer(1, answer)}
+            />
+            <Question img={require('./img/Snom.png')}
+                      opt1='Bruxish'
+                      opt2='Snom'
+                      opt3='Pikachu'
+                      onSelect={(answer) => updateAnswer(2, answer)}
+            />
 
-            {questions.map((question, index) => (
-                <Question
-                    key={index}
-                    img={question.img}
-                    options={question.options}
-                    index={index}
-                    onSelect={handleSelect}
-                />
-            ))}
-
-            <TouchableOpacity
-                onPress={checkScore}
-                style={{ backgroundColor: 'lightblue', padding: 10, margin: 20 }}
-            >
-                <Text style={{ textAlign: 'center', fontSize: 20 }}>SUBMIT</Text>
+            <TouchableOpacity onPress={checkAnswers} style={{backgroundColor: 'lightblue', padding: 15, margin: 20}}>
+                <Text style={{textAlign: 'center'}}>Submit</Text>
             </TouchableOpacity>
-        </ScrollView>
-    );
+
+        </ScrollView>)
 }
